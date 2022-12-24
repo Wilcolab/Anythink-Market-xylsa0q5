0b6304bdf45082a0c148968933cb67296054044f
vanessa-cooper-1 <ness+1@anythink.family>
2022-12-24 17:35:43 +0000

    Adding seeds script

--- backend/db/seeds.rb
+++ backend/db/seeds.rb
@@ -1,7 +1,6 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+rand_part = Random.hex[0..10]
+
+user_verified = User.create! email: "user_verified#{rand_part}@test.com", password: '12345password', username: "userverified#{rand_part}", isVerified: true
+item_verified = Item.create! title: "test item verified", description: 'verified seller item', user: user_verified, slug: 'verified_seller_item'
+user_not_verified = User.create! email: "user_not_verified#{rand_part}@test.com", password: '12345password', username: "usernotverified#{rand_part}", isVerified: false
+item_not_verified = Item.create! title: "test item not verified", description: 'not verified seller item', user: user_not_verified, slug: 'not_verified_seller_item'
